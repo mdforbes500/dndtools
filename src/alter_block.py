7e785058eb9acc25f098e1674a73bec8a37b3efa
Malcolm D. Forbes <malcolm.forbes500@gmail.com>
2018-12-29 21:12:07 -0500

    Had to get rid of malformed databases... damn sqlite.

--- src/alter_block.py
+++ src/alter_block.py
@@ -7,7 +7,7 @@ def alter_block(path):
     Takes a path to an input file, and outputs a modified text block in the
     current directory.
     """
-    
+
     filepath = str(path)
     output = open('output.txt', 'w')
     searchstr = "<!-- insert features -->"
@@ -16,8 +16,7 @@ def alter_block(path):
         while line:
             output.write(line)
             if line.strip() == searchstr:
-                output.write("> ***Fey Ancestry***. The acolyte has advantage on saving throws against being charmed, and magic can't put you to sleep.\n")
-                output.write(">\n")
+                output.write("<p><strong><em>Fey Ancestry</em></strong>. The acolyte has advantage on saving throws against being charmed, and magic can't put you to sleep.</p>\n")
             line = file.readline()
     file.close()
     output.close()
