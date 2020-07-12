540ed4f85e24b1f1f70a53d2d46be0db7a481cb1
Malcolm Forbes <malcolm.forbes500@gmail.com>
2020-07-12 18:28:08 -0400

    changed data model of system

--- back-end/test/test_npc.py
+++ back-end/test/test_npc.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-from context import classes as cs
+from context import classes
 import unittest
+import os
 
 class TestNPC(unittest.TestCase):
     """
@@ -12,47 +13,52 @@ class TestNPC(unittest.TestCase):
         """
         Sets up NPC object for testing
         """
-        self.npc = cs.npc.NPC("Test NPC")
+        self.npc = classes.npcs.npc.NPC("Test")
 
-    #=============ADD TESTS HERE \/\/\/\/===================#
-    def test_aarakocra(self):
-        aarakocra = cs.races.aarakocra(self.npc)
-        self.assertEqual(self.npc.name, "Test NPC")
+    #====================ADD TESTS HERE=====================#
+    def test_npc(self):
+        self.assertEqual(self.npc.name, "Test")
         self.assertEqual(self.npc.cr, 0)
-        self.assertEqual(self.npc.size, "Medium")
-        self.assertEqual(self.npc.type, "humanoid")
-        self.assertEqual(self.npc.race, "(aarakocra)")
-        self.assertEqual(self.npc.alignment, "neutral")
-        self.assertEqual(self.npc.speed, {"ground": 20, "fly": 50, "swim": 0, "climb": 0})
-        self.assertEqual(self.npc.abilities, [10,12,10,10,12,10])
-        self.assertEqual(self.npc.modifiers, [0,1,0,0,1,0])
-        self.assertEqual(self.npc.proficiency, 2)
-        self.assertEqual(self.npc.armor, 10)
-        self.assertEqual(self.npc.hp, 4)
-        self.assertEqual(self.npc.dicecode, "1d8")
-        self.assertEqual(self.npc.skills, {})
-        self.assertEqual(self.npc.senses, {"passive Perception": 11})
-        self.assertEqual(self.npc.languages, ["Common", "Auran"])
-        self.assertEqual(self.npc.features, {"Dive Attack": "If the aarakocra is flying and dives at least 30 feet straight toward a target and then hits it with a melee weapon attack, the attack deals an extra 3 (1d6) damage to the target"})
-        self.assertEqual(self.npc.actions, {
-            "Talon": ['Melee', 2, 5, 2, '1d4 + 0', 'piercing'],
-            "Club": ['Melee', 2, 5, 2, '1d4 + 0', 'bludgeoning']
-        })
-        self.assertEqual(self.npc.inventory, {})
+        self.assertEqual(self.npc.size, None)
+        self.assertEqual(self.npc.type, None)
+        self.assertEqual(self.npc.race, None)
+        self.assertEqual(self.npc.alignment, None)
+        self.assertEqual(self.npc.speed, {"ground": None, "fly": None, "swim": None, "climb": None})
+        self.assertEqual(self.npc.abilities, [None,None,None,None,None,None])
+        self.assertEqual(self.npc.modifiers, [None,None,None,None,None,None])
+        self.assertEqual(self.npc.proficiency, 0)
+        self.assertEqual(self.npc.armor, 0)
+        self.assertEqual(self.npc.hp, 0)
+        self.assertEqual(self.npc.dicecode, None)
+        self.assertEqual(self.npc.skills, None)
+        self.assertEqual(self.npc.senses, None)
+        self.assertEqual(self.npc.languages, None)
+        self.assertEqual(self.npc.features, None)
+        self.assertEqual(self.npc.actions, None)
+        self.assertEqual(self.npc.inventory, None)
         self.assertEqual(self.npc.cast_level, 0)
-        self.assertEqual(self.npc.caster, False)
-        self.assertEqual(self.npc.cast_ability, "")
-        self.assertEqual(self.npc.cast_save, 0)
-        self.assertEqual(self.npc.spell_attk, 0)
-        self.assertEqual(self.npc.spell_list, "")
-        self.assertEqual(self.npc.spells, [])
-        self.assertEqual(self.npc.slots, [])
+        self.assertEqual(self.npc.caster, None)
+        self.assertEqual(self.npc.cast_ability, None)
+        self.assertEqual(self.npc.cast_save, None)
+        self.assertEqual(self.npc.spell_attk, None)
+        self.assertEqual(self.npc.spell_list, None)
+        self.assertEqual(self.npc.spells, None)
+        self.assertEqual(self.npc.slots, None)
+
+    def test_export(self):
+        self.npc.export_json("test.json")
+        self.assertEqual(os.path.exists("test.json"), True)
+
+    def test_import(self):
+        self.npc.import_json("test.json")
     #=======================================================#
 
     def tearDown(self):
         """
         Cleans up after tests are run
         """
+        # if os.path.exists("test.json"):
+        #     os.remove("test.json")
         del self.npc
 
 if __name__ == "__main__":
